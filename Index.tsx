22ca25046a852f13b7319a2d2f73d459083eed62
aiky13 <aikllmy13@gmail.com>
2025-11-12 19:20:43 -0300

    Set up routing with Login and Cadastro pages

---
+++ Index.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Cadastro from "./pages/Cadastro";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
